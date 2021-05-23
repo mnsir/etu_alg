68f4c0393efb5fd38e9ac412b325aadafddce9e1
mnsir <mnsir@ya.ru>
2021-05-23 03:14:29 +0300

    second commit

---
+++ CourseProject/Application/ApplicationDlg.h
@@ -0,0 +1,33 @@
+﻿
+// ApplicationDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CApplicationDlg
+class CApplicationDlg : public CDialogEx
+{
+// Создание
+public:
+	CApplicationDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_APPLICATION_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
